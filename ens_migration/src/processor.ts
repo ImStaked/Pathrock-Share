00b375f2340ef54317c092201c820f49e781afa2
ImStaked <82274336+ImStaked@users.noreply.github.com>
2023-09-30 16:37:28 -0500

    Update processor.ts

--- ens_migration/src/processor.ts
+++ ens_migration/src/processor.ts
@@ -8,11 +8,11 @@ import {
     Transaction as _Transaction,
 } from '@subsquid/evm-processor'
 import * as EnsRegistry from './abi/AuctionRegistrar'
-import * as ENS_resolver from './abi/PublicResolver' 
-import * as ENS_baseregistrar from './abi/BaseRegistrar'
-import * as ENS_registrar_controller_old from './abi/EthRegistrarControllerOld'
-import * as ENS_registrar_controller from './abi/EthRegistrarController'
-import * as ENS_namewrapper from './abi/NameWrapper'
+import * as EnsResolver from './abi/PublicResolver' 
+import * as EnsBaseregistrar from './abi/BaseRegistrar'
+import * as EnsRegistrarControllerOld from './abi/EthRegistrarControllerOld'
+import * as EnsRegistrarController from './abi/EthRegistrarController'
+import * as EnsNamewrapper from './abi/NameWrapper'
 
 export const processor = new EvmBatchProcessor()
     .setDataSource({
@@ -49,63 +49,63 @@ export const processor = new EvmBatchProcessor()
     .addLog({
         address: ['0x314159265dd8dbb310642f98f50c066173c1259b'],
         topic1: [
-          ENS_resolver.events.ABIChanged.topic,
-          ENS_resolver.events.AddrChanged.topic,
-          ENS_resolver.events.AddressChanged.topic,
-          ENS_resolver.events.AuthorisationChanged.topic,
-          ENS_resolver.events.ContenthashChanged.topic,
-          ENS_resolver.events.InterfaceChanged.topic,
-          ENS_resolver.events.NameChanged.topic,
-          ENS_resolver.events.PubkeyChanged.topic,
-          ENS_resolver.events['TextChanged(bytes32,string,string)'].topic,
-          ENS_resolver.events['TextChanged(bytes32,string,string,string)'].topic,
-          ENS_resolver.events.VersionChanged.topic,
+          EnsResolver.events.ABIChanged.topic,
+          EnsResolver.events.AddrChanged.topic,
+          EnsResolver.events.AddressChanged.topic,
+          EnsResolver.events.AuthorisationChanged.topic,
+          EnsResolver.events.ContenthashChanged.topic,
+          EnsResolver.events.InterfaceChanged.topic,
+          EnsResolver.events.NameChanged.topic,
+          EnsResolver.events.PubkeyChanged.topic,
+          EnsResolver.events['TextChanged(bytes32,string,string)'].topic,
+          EnsResolver.events['TextChanged(bytes32,string,string,string)'].topic,
+          EnsResolver.events.VersionChanged.topic,
         ]
     })
     .addLog({
         address: ['0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85'],
         topic1: [
-          ENS_baseregistrar.events.NameRegistered.topic,
-          ENS_baseregistrar.events.NameRenewed.topic,
-          ENS_baseregistrar.events.Transfer.topic,
-          ENS_baseregistrar.events.Approval.topic,
-          ENS_baseregistrar.events.ApprovalForAll.topic,
-          ENS_baseregistrar.events.ControllerAdded.topic,
-          ENS_baseregistrar.events.ControllerRemoved.topic,
-          ENS_baseregistrar.events.NameMigrated.topic,
-          ENS_baseregistrar.events.OwnershipTransferred.topic,
+          EnsBaseregistrar.events.NameRegistered.topic,
+          EnsBaseregistrar.events.NameRenewed.topic,
+          EnsBaseregistrar.events.Transfer.topic,
+          EnsBaseregistrar.events.Approval.topic,
+          EnsBaseregistrar.events.ApprovalForAll.topic,
+          EnsBaseregistrar.events.ControllerAdded.topic,
+          EnsBaseregistrar.events.ControllerRemoved.topic,
+          EnsBaseregistrar.events.NameMigrated.topic,
+          EnsBaseregistrar.events.OwnershipTransferred.topic,
         ]
     })
     .addLog({
         address: ['0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5'],
         topic1: [
-          ENS_registrar_controller_old.events.NameRegistered.topic,
-          ENS_registrar_controller_old.events.NameRenewed.topic,
-          ENS_registrar_controller_old.events.NewPriceOracle.topic,
-          ENS_registrar_controller_old.events.OwnershipTransferred.topic,
+          EnsRegistrarControllerOld.events.NameRegistered.topic,
+          EnsRegistrarControllerOld.events.NameRenewed.topic,
+          EnsRegistrarControllerOld.events.NewPriceOracle.topic,
+          EnsRegistrarControllerOld.events.OwnershipTransferred.topic,
         ]
     })
     .addLog({
         address: ['0x253553366Da8546fC250F225fe3d25d0C782303b'],
         topic1: [
-          ENS_registrar_controller.events.NameRegistered.topic,
-          ENS_registrar_controller.events.NameRenewed.topic,
-          ENS_registrar_controller.events.OwnershipTransferred.topic,
+          EnsRegistrarController.events.NameRegistered.topic,
+          EnsRegistrarController.events.NameRenewed.topic,
+          EnsRegistrarController.events.OwnershipTransferred.topic,
         ]
     })
     .addLog({
         address: ['0xD4416b13d2b3a9aBae7AcD5D6C2BbDBE25686401'],
         topic0: [
-          ENS_namewrapper.events.NameWrapped.topic,
-          ENS_namewrapper.events.NameUnwrapped.topic,
-          ENS_namewrapper.events.FusesSet.topic,
-          ENS_namewrapper.events.ExpiryExtended.topic,
-          ENS_namewrapper.events.TransferSingle.topic,
-          ENS_namewrapper.events.TransferBatch.topic,
-          ENS_namewrapper.events.ApprovalForAll.topic,
-          ENS_namewrapper.events.ControllerChanged.topic,
-          ENS_namewrapper.events.OwnershipTransferred.topic,
-          ENS_namewrapper.events.URI.topic,
+          EnsNamewrapper.events.NameWrapped.topic,
+          EnsNamewrapper.events.NameUnwrapped.topic,
+          EnsNamewrapper.events.FusesSet.topic,
+          EnsNamewrapper.events.ExpiryExtended.topic,
+          EnsNamewrapper.events.TransferSingle.topic,
+          EnsNamewrapper.events.TransferBatch.topic,
+          EnsNamewrapper.events.ApprovalForAll.topic,
+          EnsNamewrapper.events.ControllerChanged.topic,
+          EnsNamewrapper.events.OwnershipTransferred.topic,
+          EnsNamewrapper.events.URI.topic,
         ]
     })
 
