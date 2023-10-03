16789ce7eca501a61e5d70a0ac948fe25ee4f078
ImStaked <82274336+ImStaked@users.noreply.github.com>
2023-10-02 19:02:56 -0500

    Update main.ts

--- ens_migration/src/main.ts
+++ ens_migration/src/main.ts
@@ -1,10 +1,10 @@
 import {TypeormDatabase} from '@subsquid/typeorm-store'
-import {Domain} from './model'
-import {Registration} from './model'
-import {WrappedDomain} from './model'
-import {Account} from './model'
-import {Resolver} from './model'
+import {DomainEvent} from './model'
+import {} from './model'
+import {} from './model'
+import {} from './model'
 import {processor} from './processor'
+
 import * as EsnRegistryABI from './abi/Registry'
 import * as EnsResolverABI from './abi/PublicResolver' 
 import * as EnsBaseRegistrarABI from './abi/BaseRegistrar'
@@ -13,22 +13,34 @@ import * as EnsRegistrarControllerABI from './abi/EthRegistrarController'
 import * as ENSNamewrapperABI from './abi/NameWrapper'
 
 processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
-    const Domains: Domain[] = []
+    const DomainEvents: DomainEvent[] = []
     for (let c of ctx.blocks) {
         for (let log of c.logs) {
-          if(log.topics[0] === EsnRegistryABI.events.NewOwner.topic){
+        if(log.topics[0] === EsnRegistryABI.events.NewOwner.topic){
             let {node, label, owner} = EsnRegistryABI.events.NewOwner.decode(log); 
-            let newowner = Domain({
+            let newowner = DomainEvent({
                 node: node,
                 label: label,
                 owner: owner, 
-            });
-            if(log.topics[0] === EsnRegistryABI.events.NewResolver.topic){
+                });
+        if(log.topics[0] === EsnRegistryABI.events.NewResolver.topic){
                 let {node, resolver} = EsnRegistryABI.events.NewResolver.decode(log); 
-                let newresolver = Domain({
+                let newresolver = DomainEvent({
                     node: node,
                     resolver: resolver, 
-                });    
+                });   
+        if(log.topics[0] === EsnRegistryABI.events.NewTTL.topic){
+                let {node,ttl} = EsnRegistryABI.events.NewTTL.decode(log); 
+                let newowner = DomainEvent({
+                    node: node,
+                    ttl: ttl,
+                });
+        if(log.topics[0] === EsnRegistryABI.events.Transfer.topic){
+                let {node,owner} = EsnRegistryABI.events.Transfer.decode(log); 
+                let newowner = DomainEvent({
+                    node: node,
+                    owner: owner,
+                });
         }
           }
         }
