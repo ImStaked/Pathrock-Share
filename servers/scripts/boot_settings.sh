7cb12042cc300a739385920194c8454d982bd9a8
ImStaked <82274336+ImStaked@users.noreply.github.com>
2023-10-31 15:30:45 -0500

    Create boot_settings.sh

---
+++ servers/scripts/boot_settings.sh
@@ -0,0 +1,25 @@
+cpufreq-set -g performance -c 0
+cpufreq-set -g performance -c 1
+cpufreq-set -g performance -c 2
+cpufreq-set -g performance -c 3
+cpufreq-set -g performance -c 4
+cpufreq-set -g performance -c 6
+cpufreq-set -g performance -c 7
+cpufreq-set -g performance -c 8
+cpufreq-set -g performance -c 9
+cpufreq-set -g performance -c 10
+cpufreq-set -g performance -c 11
+cpufreq-set -g performance -c 12
+cpufreq-set -g performance -c 13
+cpufreq-set -g performance -c 14
+cpufreq-set -g performance -c 15
+cpufreq-set -g performance -c 16
+cpufreq-set -g performance -c 17
+cpufreq-set -g performance -c 18
+cpufreq-set -g performance -c 19
+cpufreq-set -g performance -c 20
+cpufreq-set -g performance -c 21
+cpufreq-set -g performance -c 22
+cpufreq-set -g performance -c 23
+
+cpupower idle-set -d 2
